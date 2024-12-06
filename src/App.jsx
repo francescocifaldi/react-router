ea8d45a329a546e9f77b978c33d0e835b1332ede
francescocifaldi <francesco.cifaldi@gmail.com>
2024-12-06 13:51:46 +0100

    ADD routes and default layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import DefaultLayout from './layouts/DefaultLayout'
+import Home from './pages/Home'
+import AboutUS from './pages/AboutUs'
+import Content from './pages/Content'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,10 +12,13 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route />
-          <Route />
+          <Route element={<DefaultLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/content" element={<Content />} />
+            <Route path="/about-us" element={<AboutUS />} />
+          </Route>
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter >
     </>
   )
 }
--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,5 +1,11 @@
+import Nav from "../components/Nav";
+import { Outlet } from "react-router-dom";
+
 export default function DefaultLayout() {
     return (
-        <></>
+        <>
+            < Nav />
+            <Outlet />
+        </>
     )
 }
