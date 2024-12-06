a5cbb59450411d9c69b1a9383c7c68599c9f57c1
francescocifaldi <francesco.cifaldi@gmail.com>
2024-12-06 14:30:34 +0100

    ADD navbar in header and posts array

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,16 @@
 import { Link, NavLink } from "react-router-dom"
+import style from './Nav.module.css'
 
 export default function Nav() {
     return (
         <>
             <nav>
                 <div className="container">
-                    <div className="menu">
-                        <ul>
-                            <NavLink to="/">Home</NavLink>
-                            <NavLink to="/content">Content</NavLink>
-                            <NavLink to="/about-us">About us</NavLink>
+                    <div className={style.navbar}>
+                        <ul className={style.menu}>
+                            <li><NavLink to="/">Home</NavLink></li>
+                            <li><NavLink to="/content">Content</NavLink></li>
+                            <li><NavLink to="/about-us">About us</NavLink></li>
                         </ul>
                     </div>
                 </div>
---
+++ src/posts.js
@@ -0,0 +1,28 @@
+export default posts = [
+    {
+        id: 1,
+        titolo: "Introduzione a JavaScript",
+        descrizione: "Scopri le basi di JavaScript, il linguaggio di programmazione per il web."
+    },
+    {
+        id: 2,
+        titolo: "CSS per principianti",
+        descrizione: "Impara come stilizzare le tue pagine web con CSS."
+    },
+    {
+        id: 3,
+        titolo: "Guida a HTML5",
+        descrizione: "Una panoramica delle nuove funzionalità di HTML5 per creare pagine web moderne."
+    },
+    {
+        id: 4,
+        titolo: "Sviluppo Web Responsive",
+        descrizione: "Come rendere i tuoi siti web accessibili su tutti i dispositivi."
+    },
+    {
+        id: 5,
+        titolo: "Introduzione a Node.js",
+        descrizione: "Inizia a sviluppare applicazioni lato server con Node.js."
+    }
+];
+
