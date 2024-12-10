511d411779c2b0ee23dad671b5fd4642cc03fbd6
francescocifaldi <francesco.cifaldi@gmail.com>
2024-12-10 09:48:24 +0100

    ADD 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import DefaultLayout from './layouts/DefaultLayout'
 import Home from './pages/Home'
 import AboutUS from './pages/AboutUs'
+import BlankPage from './pages/BlankPage'
 import Content from './pages/Content'
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
             <Route path="/content" element={<Content />} />
             <Route path="/about-us" element={<AboutUS />} />
           </Route>
+          <Route path='*' element={<BlankPage />} />
         </Routes>
       </BrowserRouter >
     </>
---
+++ src/pages/BlankPage.jsx
@@ -0,0 +1,5 @@
+export default function BlankPage() {
+    return (
+        <div>Error! Page not found</div>
+    )
+}
